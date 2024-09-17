02a2a7f2fe550f97d535381176834ee901671ded
Carl-Montpetit <montpetit.carl@icloud.com>
2024-09-16 22:01:58 -0400

    Initial code

---
+++ src/main/java/com/traveljournal/traveljournalbff/TravelJournalBffApplication.java
@@ -0,0 +1,13 @@
+package com.traveljournal.traveljournalbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelJournalBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelJournalBffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/traveljournal/traveljournalbff/TravelJournalBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traveljournal.traveljournalbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelJournalBffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
